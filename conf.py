72d6547a6e70b239895d4245b6a51dc6824ddc3f
Armin Stebich <home@mail.lordofbikes.de>
2019-02-07 20:02:26 +0100

    rename _static folder because it is on ignore list

--- conf.py
+++ conf.py
@@ -114,7 +114,7 @@ html_static_path = ['_static']
 # Table width fix for Read the Docs Sphinx theme
 html_context = {
     'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        'static/theme_overrides.css',  # override wide tables in RTD theme
         ],
      }
 
