05dbfddaf090c3fa0e9eec077b689d551575a6d4
GSStnb <44550894+GSStnb@users.noreply.github.com>
2019-05-23 13:31:07 -0600

    Alt LibreCAD color theme method test 6

--- conf.py
+++ conf.py
@@ -85,8 +85,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 ##html_theme = 'alabaster'
-##html_theme = 'sphinx_rtd_theme'
-html_theme = 'classic'
+##html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -144,7 +144,7 @@ htmlhelp_basename = 'LibreCADdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+     'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
