4d47a610361ae2387617026e91d2da1178156370
flywire <flywire0@gmail.com>
2020-01-21 16:08:35 +1100

    changed sphinx-quickstart for Windows

--- make.bat
+++ make.bat
@@ -13,6 +13,11 @@ set SPHINXPROJ=LibreCAD
 
 if "%1" == "" goto help
 
+if "%1" == "livehtml" (
+	sphinx-autobuild -b html %SOURCEDIR% %BUILDDIR%\html
+	goto end
+)
+
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
